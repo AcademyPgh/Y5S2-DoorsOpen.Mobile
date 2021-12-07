4491e2665d81068b398865c94b24a9c652ca2340
echo storrs <echoatday@gmail.com>
2021-12-07 11:27:22 -0500

    Clickable building images and basic home layout.

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
 import React, { Component, useEffect, useState } from "react";
-import { Button, View, Text, FlatList, ScrollView, ActivityIndicator, ImageBackground } from 'react-native';
+import { Button, View, Text, FlatList, ScrollView, ActivityIndicator, ImageBackground, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const styles = StyleSheet.create({
+  homeFlatList: {
+    width: '100%'
+  },
+  homeBuildingDisplay: {
+    width: '100%',
+    height: 300,
+    marginBottom: 10,
+    objectFit: 'cover',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  homeBuildingCard: {
+    width: '90%',
+    backgroundColor: 'white',
+    margin: 10,
+  }
+})
 
 function ListScreen({ route, navigation }) {
 
@@ -29,10 +49,24 @@ function ListScreen({ route, navigation }) {
       <Text>List of Buildings!</Text>
       {isLoading ? <ActivityIndicator/> : (
       <FlatList
+        style={styles.homeFlatList}
         data={data}
         renderItem={
-          ({item}) => <ImageBackground source={{uri: item.imageURL}} style={{width: '100%', height: '50%'}}>
-            <Text>{item.building}</Text>
+          ({item}) => 
+          <TouchableWithoutFeedback 
+          onPress={() => {
+            navigation.navigate('Details', {
+              building: item.building,
+              historicalOverview: item.historicalOverview,
+              visitorExperience: item.visitorExperience,
+            });
+          }}>
+          <ImageBackground source={{uri: item.imageURL}} style={styles.homeBuildingDisplay}>
+            <View style={styles.homeBuildingCard}>
+              <Text>{item.building}</Text>
+              <Text>{item.address1} | {item.startTime}-{item.endTime}</Text>
+              <Text>Accessibility Icons{item.wheelchairAccessible}{item.restroomsAvailable}{item.photographyAllowed}</Text>
+            </View>
             {/* Image
             name
             address
@@ -41,7 +75,7 @@ function ListScreen({ route, navigation }) {
               toilet
               wheelchair
               photography */}
-            <Button
+            {/* <Button
         title="Go to Details"
         onPress={() => {
           navigation.navigate('Details', {
@@ -50,8 +84,9 @@ function ListScreen({ route, navigation }) {
             visitorExperience: item.visitorExperience,
           });
         }}
-      />
+      /> */}
             </ImageBackground>
+            </TouchableWithoutFeedback>
         }
       />
       )}
@@ -66,7 +101,7 @@ function DetailsScreen({ route, navigation }) {
       <Text>{route.params.building}</Text>
       <Text>{route.params.historicalOverview}</Text>
       <Text>{route.params.visitorExperience}</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      {/* <Button title="Go back" onPress={() => navigation.goBack()} /> */}
     </View>
   );
 }
